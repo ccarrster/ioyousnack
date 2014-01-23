7574f3bc2c9c91b7b96f86d4eabaedeb16325075
Chris Carr <chris@petunia.local>
2014-01-23 18:09:38 -0500

    Manage

---
+++ src/manage.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+<?php
+if(isset($_POST['action']) && $_POST['action'] == 'createEater'){
+	$name = $_POST['name'];
+	$picture = '';
+	if(isset($_FILES["picture"])){
+		move_uploaded_file($_FILES["picture"]["tmp_name"], "eaters/" . $_FILES["picture"]["name"]);
+		$picture = $_FILES["picture"]["name"];
+	}
+	$link = mysql_connect('localhost', 'root', 'F3ckth1s');
+	mysql_select_db('mysql');
+	$query = "insert into eater (name, picture, debt) values('".$name.", '".$picture."', 0);";
+	mysql_query($query);
+	echo('Created User ' . $name);
+}
+?>
+Create User</br>
+<form method="POST" enctype="multipart/form-data">
+<input type="text" name="name"/></br>
+<input type="file" name="picture"/></br>
+<input type="hidden" action="createEater"/>
+<input type="submit"/></br>
+</form>
+</body>
+</html>
