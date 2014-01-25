1fa60d594cc92b51a9a2d3629cd4531f5c16651b
Chris Carr <chris@petunia.gateway.2wire.net>
2014-01-25 01:46:09 -0500

    Done

--- src/persist.php
+++ src/persist.php
@@ -1,8 +1,10 @@
 <?php
 	$id = $_GET['id'];
 	$price = $_GET['price'];
-	$link = mysql_connect('localhost', 'root', 'F3ckth1s');
-	mysql_select_db('mysql');
-	$query = "update eater set debt = debt + " . $price . " where id = " . $id;
-	mysql_query($query);
+	if(preg_match("/^[0-9]+$/i", $id) && preg_match("/^-?[0-9]+$/i", $price)){
+		$link = mysql_connect('localhost', 'root', 'F3ckth1s');
+		mysql_select_db('mysql');
+		$query = "update eater set debt = debt + " . $price . " where id = " . $id;
+		mysql_query($query);
+	}
 ?>
