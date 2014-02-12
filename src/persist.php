41a1a4ceb74a6b11e3fe874bea9b71ed2dc1f080
Chris Carr <chris@petunia.(none)>
2014-02-12 05:40:56 -0500

    echo

--- src/persist.php
+++ src/persist.php
@@ -17,7 +17,7 @@
 			}
 			$query = "insert into buypaylog (eaterid, eatid, delta, debt, exchangeTime) values('" . $id . "', '" . $productid . "', '" . $price . "', '" . $debt . "', now());";
 			mysql_query($query);
-			return $row['debt'];
+			echo($row['debt']);
 		}
 	}
 
