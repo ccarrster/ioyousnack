70a3336be1f1e432b0ecbf39903c30ca1e67758d
Chris Carr <chris@petunia.local>
2014-02-19 17:06:10 -0500

    consume

--- src/report.php
+++ src/report.php
@@ -6,13 +6,24 @@ require_once('config.php');
 <?php
 			$link = mysql_connect($dbUrl, $dbUser, $dbPassword);
 			mysql_select_db($dbName);
+			echo('Hour of Consumption Report<br/>');
 			$query = "select HOUR(exchangeTime) as gmthour, count(*) as count from buypaylog group by HOUR(exchangeTime);";
 			$result = mysql_query($query);
 			$arrayIndex = 0;
+			$hours = array();
+			for($i = 0; $i < 24; $i++){
+				$hours[$i] = 0;
+			}
+
 			while($row = mysql_fetch_array( $result )) {
-				echo($row['gmthour']);
-				echo($row['count']);
-				echo('<br/>');
+				$hours[$row['gmthour']] = $row['count'];
+			}
+
+			for($i = 5; $i < 24; $i++){
+				echo($i . ' ' . $hours[$i] . '<br/>');
+			}
+			for($i = 0; $i < 6; $i++){
+				echo((19 + $i) . ' ' . $hours[$i] . '<br/>');
 			}
 ?>
 </body>
