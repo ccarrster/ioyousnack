46092e2105ed83c9029599be13bd8223ea271af9
Chris Carr <chris@petunia.local>
2014-02-19 17:13:14 -0500

    Collection

--- src/report.php
+++ src/report.php
@@ -7,7 +7,27 @@ require_once('config.php');
 			$link = mysql_connect($dbUrl, $dbUser, $dbPassword);
 			mysql_select_db($dbName);
 			echo('Hour of Consumption Report<br/>');
-			$query = "select HOUR(exchangeTime) as gmthour, count(*) as count from buypaylog group by HOUR(exchangeTime);";
+			$query = "select HOUR(exchangeTime) as gmthour, count(*) as count from buypaylog where delta > 0 group by HOUR(exchangeTime);";
+			$result = mysql_query($query);
+			$arrayIndex = 0;
+			$hours = array();
+			for($i = 0; $i < 24; $i++){
+				$hours[$i] = 0;
+			}
+
+			while($row = mysql_fetch_array( $result )) {
+				$hours[$row['gmthour']] = $row['count'];
+			}
+
+			for($i = 5; $i < 24; $i++){
+				echo($i - 5 . ' ' . $hours[$i] . '<br/>');
+			}
+			for($i = 0; $i < 5; $i++){
+				echo((19 + $i) . ' ' . $hours[$i] . '<br/>');
+			}
+			echo('<br/>');
+			echo('Hour of Collection Report<br/>');
+			$query = "select HOUR(exchangeTime) as gmthour, count(*) as count from buypaylog where delta < 0 group by HOUR(exchangeTime);";
 			$result = mysql_query($query);
 			$arrayIndex = 0;
 			$hours = array();
