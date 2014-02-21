343e0cc7421fab5c25ce588a382e12630f84dbdb
Chris Carr <chris@petunia.local>
2014-02-21 12:01:39 -0500

    image

--- src/mustachize.php
+++ src/mustachize.php
@@ -3,7 +3,7 @@ var_dump($_FILES);
 ?>
 <html>
 <body>
-<form enctype="multipart/form-data">
+<form enctype="multipart/form-data" method="post">
 	<input type="file" name="face">
 	<input type="submit"/>
 </form>
