19e9beec8a56760d7953da4d9fb71e84eeb48da9
Chris Carr <chris@petunia.local>
2014-02-21 12:12:53 -0500

    stash

--- src/mustachize.php
+++ src/mustachize.php
@@ -1,4 +1,5 @@
 <?php
+require_once('config.php');
 if(isset($_FILES['face'])){
 	move_uploaded_file($_FILES["face"]["tmp_name"], 'eaters/' . $_FILES["face"]["name"]);
 	$fileName = 'eaters/' . $_FILES["face"]["name"];
@@ -9,6 +10,8 @@ if(isset($_FILES['face'])){
 <?php
 if(isset($fileName)){
 	echo('<img src="'.$fileName.'"/>');
+	$mustachify = 'http://mustachify.me/?src=' . $siteUrl . '/';
+	echo('<img src="'.$mustachify.$fileName.'"/>');
 }
 ?>
 <form enctype="multipart/form-data" method="post">
