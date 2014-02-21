0a48a99b8dd39ebb06c1b4af4322cad26b70043a
Chris Carr <chris@petunia.local>
2014-02-21 12:06:20 -0500

    Ize

--- src/mustachize.php
+++ src/mustachize.php
@@ -1,8 +1,16 @@
 <?php
-var_dump($_FILES);
+if(isset($_FILES['face'])){
+	move_uploaded_file($_FILES["face"]["tmp_name"], $_FILES["face"]["name"]);
+	$fileName = $_FILES["face"]["name"];
+}
 ?>
 <html>
 <body>
+<?php
+if(isset($fileName)){
+	echo('<img src="'.$fileName.'"/>');
+}
+?>
 <form enctype="multipart/form-data" method="post">
 	<input type="file" name="face">
 	<input type="submit"/>
