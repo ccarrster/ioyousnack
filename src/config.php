f4a3e3ed92f4caa1dee73b0f68d1b587cf16632a
Chris Carr <chris@petunia.local>
2014-04-24 13:28:28 -0400

    config load whitelist name and url

--- src/config.php
+++ src/config.php
@@ -6,4 +6,14 @@ $dbName = 'mysql';
 $appName = 'Snack Tracker';
 $ipWhiteList = '';
 $siteUrl = '';
+
+$link = mysql_connect($dbUrl, $dbUser, $dbPassword);
+mysql_select_db($dbName);
+$result = mysql_query("select ipwhitelist, name from store where id = $storeId;");
+$arrayIndex = 0;
+while($row = mysql_fetch_array( $result )) {
+	$ipWhiteList = $row['ipwhitelist'];
+	$appName = $row['name'];
+	$siteUrl = $row['url'];
+}
 ?>
